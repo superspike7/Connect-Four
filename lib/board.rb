32fa6426bf6c893343f4c4516a25a8e2c2cc5ad8
Spike Vinz <spikevinz@gmail.com>
2020-11-23 09:40:02 +0800

    changing diagonals to constants

--- lib/board.rb
+++ lib/board.rb
@@ -51,50 +51,25 @@ class Board
     true
   end
 
-  def four_in_a_row_by_row(arr)
-    arr.each do |row|
-      a = row.each_cons(4).find { |a| a.uniq.size == 1 && a.first != '.'}
-      return a.first unless a.nil?
-    end
-    return nil
-  end
+ def diagonals
+# TODO : 
+# write constant diagonals 
+ end
 
-  def diagonals(grid)
-   (0..grid.size-4).map do |i|
-    (0..grid.size-1-i).map { |j| grid[i+j][j] }
-    end.concat((1..grid.first.size-4).map do |j|
-      (0..grid.size-j-1).map { |i| grid[i][j+i] }
-    end)
-  end
 
-  def rotate90(grid=@grid)
-  ncols = grid.first.size
-  grid.each_index.with_object([]) do |i,a|
-    a << ncols.times.map { |j| grid[j][ncols-1-i] }
-  end
-end
+ 
 
 
 end
 
 board = Board.new
 board.grid = [
-          ['1', '4', '5', '6', 'x', '.', 'O'],
-          ['2', '1', '4', '5', '6', '3', 'X'],
-          ['3', '2', '1', '4', '5', '6', 'O'],
-          ['3', '3', '2', '1', '4', '5', '6'],
-          ['.', 'O', '3', '2', '1', '4', '5'],
-          ['O', 'O', 'X', '3', '2', '1', '4']
-        ]
-# p board.four_in_a_row_by_row
+      ['.', 'O', '.', 'X', '.', '.', 'O'],
+      ['.', '.', 'X', '.', '.', '.', 'X'],
+      ['.', 'X', '.', 'O', '.', '.', 'O'],
+      ['X', '.', '.', '.', 'O', '.', 'O'],
+      ['.', '.', '.', '.', '.', 'O', 'X'],
+      ['.', 'O', 'X', 'O', 'X', 'O', 'O']
+     ]
 
-# row = ['O', 'O', 'X', 'O', 'X', 'O', 'O']
-# enum0 = row.each_cons(4)
-# enum1 = enum0.find
-# p enum1.to_a
-# p a = enum1.next
-# p u = a.uniq
-# p u = u.size == 1
-p board.diagonals(board.grid) 
-p board.rotate90
-p board.diagonals(board.gird)
+p board.diagonals_list
